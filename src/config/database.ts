193ee56388fe9189e89e9ed59aa1fcb7e8c96bbc
dada-mhmd <moudi.ld@hotmail.com>
2024-08-18 14:43:54 +0300

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error('MONGO_URI is not defined in the environment variables');
-    }
+    // if (!process.env.MONGO_URI) {
+    //   throw new Error('MONGO_URI is not defined in the environment variables');
+    // }
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB database');
   } catch (error) {
